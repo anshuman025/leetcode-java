68abc904fbafed3b3b87dcef17d67a637c6721f0
Anshuman Sharma <ajrksharma@gmail.com>
2026-08-04 23:32:51 +0530

    Added LeetCode 1252: Cells with Odd Values in a Matrix

---
+++ Arrays/1252_Cells_with_Odd_Values_in_a_Matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int[][] a  =  indices;
+        int [][] matrix = new int[m][n];
+
+        for(int i = 0; i < indices.length;i++){
+
+            int r = a[i][0];
+            int c = a[i][1];
+
+            for(int j = 0; j< n;j++){
+                matrix[r][j]++;
+            }
+
+            for(int j = 0; j< m;j++){
+                matrix[j][c]++;
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i < m;i++){
+            for(int j = 0; j< n;j++){
+                if(matrix[i][j] % 2 != 0){
+                    count++;
+                }
+            }
+        }
+        return count;
+
+    }
+}
