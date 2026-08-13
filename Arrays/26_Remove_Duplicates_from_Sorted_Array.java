ad7ce1e1283b3cc65503082d35751d068e6d7052
Anshuman Sharma <ajrksharma@gmail.com>
2026-08-13 22:57:10 +0530

    Updated LeetCode 26: Added second two-pointer approach

--- Arrays/26_Remove_Duplicates_from_Sorted_Array.java
+++ Arrays/26_Remove_Duplicates_from_Sorted_Array.java
@@ -1,3 +1,22 @@
+/*
+ * LeetCode 26 - Remove Duplicates from Sorted Array
+ * Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ *
+ * ==========================================================
+ * Approach 1 - i = 0
+ * ==========================================================
+ *
+ * i → index of the last unique element
+ * j → scans the array
+ *
+ * When a new unique number is found:
+ * 1. Move i forward
+ * 2. Put the new number at nums[i]
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
@@ -16,5 +35,45 @@ class Solution {
         return i+1;
 
 
+    }
+}
+
+
+/*
+ * ==========================================================
+ * Approach 2 - i = 1
+ * ==========================================================
+ *
+ * i → next position where a unique number should be placed
+ * j → scans the array
+ *
+ * When a new unique number is found:
+ * 1. Put the number at nums[i]
+ * 2. Move i forward
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int n = nums.length;
+
+        if (n == 0) {
+            return 0;
+        }
+
+        int i = 1;
+
+        for (int j = 1; j < n; j++) {
+
+            if (nums[j] != nums[i - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
     }
 }
