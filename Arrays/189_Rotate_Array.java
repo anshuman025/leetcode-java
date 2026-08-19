0fd096e2fc76086b0195114a685a7613997fc7b5
Anshuman Sharma <ajrksharma@gmail.com>
2026-08-19 23:26:31 +0530

    Updated LeetCode 189: Added optimized reversal approach

--- Arrays/189_Rotate_Array.java
+++ Arrays/189_Rotate_Array.java
@@ -4,7 +4,8 @@
  * Link:
  * https://leetcode.com/problems/rotate-array/
  *
- * Approach:
+ * ----------------------------------------------------
+ * Approach 1: Brute Force + Modulo
  *
  * Rotate the array one position at a time.
  *
@@ -18,10 +19,41 @@
  * Optimization:
  * Partially improved by reducing unnecessary rotations.
  * Further optimization is possible.
+ * ----------------------------------------------------
+ *
+ * Approach 2: Optimized Reversal
+ *
+ * 1. Reduce k using modulo.
+ * 2. Reverse the entire array.
+ * 3. Reverse the first k elements.
+ * 4. Reverse the remaining elements.
+ *
+ * Example:
+ *
+ * nums = [1,2,3,4,5,6,7]
+ * k = 3
+ *
+ * Reverse whole:
+ * [7,6,5,4,3,2,1]
+ *
+ * Reverse first k:
+ * [5,6,7,4,3,2,1]
+ *
+ * Reverse remaining:
+ * [5,6,7,1,2,3,4]
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ *
+ * Optimization:
+ * Fully optimized.
+ * ----------------------------------------------------
  */
 
 class Solution {
-    public void rotate(int[] nums, int k) {
+
+    // Approach 1: Brute Force + Modulo
+    public void rotateBruteForce(int[] nums, int k) {
 
         k %= nums.length;
 
@@ -34,6 +66,32 @@ class Solution {
             }
 
             nums[0] = last;
+        }
+    }
+
+
+    // Approach 2: Optimized Reversal
+    public void rotate(int[] nums, int k) {
+
+        k %= nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+
+        reverse(nums, 0, k - 1);
+
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int s, int e) {
+
+        while (s < e) {
+
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+
+            s++;
+            e--;
         }
     }
 }
