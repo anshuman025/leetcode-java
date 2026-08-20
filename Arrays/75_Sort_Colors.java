df47c18e3500efc85ad6545a0bc184bbf41e0d06
Anshuman Sharma <ajrksharma@gmail.com>
2026-08-20 22:17:02 +0530

    Updated LeetCode 75: Added Dutch National Flag approach

--- Arrays/75_Sort_Colors.java
+++ Arrays/75_Sort_Colors.java
@@ -4,40 +4,53 @@
  * Link:
  * https://leetcode.com/problems/sort-colors/
  *
- * Approach:
+ * ----------------------------------------------------
+ * Approach 1: Counting
  *
- * Count the number of 0s, 1s, and 2s in the array.
+ * Count the number of 0s, 1s, and 2s.
  * Then overwrite the array using those counts.
  *
- * Example:
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
  *
- * nums = [2,0,2,1,1,0]
+ * ----------------------------------------------------
+ * Approach 2: Dutch National Flag
  *
- * Count:
- * 0 -> 2
- * 1 -> 2
- * 2 -> 2
+ * Use three pointers:
  *
- * Result:
- * [0,0,1,1,2,2]
+ * low  -> position for 0
+ * mid  -> current element being checked
+ * high -> position for 2
+ *
+ * If nums[mid] == 0:
+ * Move it to the left.
+ *
+ * If nums[mid] == 1:
+ * Move mid forward.
+ *
+ * If nums[mid] == 2:
+ * Move it to the right.
  *
  * Time Complexity: O(n)
  * Space Complexity: O(1)
  *
  * Optimization:
- * This solution is O(n) time and O(1) space.
- * A one-pass Dutch National Flag approach can
- * be explored as another approach.
+ * Fully optimized one-pass solution.
+ * ----------------------------------------------------
  */
 
 class Solution {
-    public void sortColors(int[] nums) {
+
+    // Approach 1: Counting
+
+    public void sortColorsCounting(int[] nums) {
 
         int zero = 0;
         int one = 0;
         int two = 0;
 
         for (int num : nums) {
+
             if (num == 0) {
                 zero++;
             } else if (num == 1) {
@@ -63,5 +76,41 @@ class Solution {
             nums[i++] = 2;
             two--;
         }
+    }
+
+
+    // Approach 2: Dutch National Flag
+
+    public void sortColors(int[] nums) {
+
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+
+            if (nums[mid] == 0) {
+
+                swap(nums, low, mid);
+                low++;
+                mid++;
+
+            } else if (nums[mid] == 1) {
+
+                mid++;
+
+            } else {
+
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
