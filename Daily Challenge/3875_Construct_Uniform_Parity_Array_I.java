44a10d1ad21f19d129516faa048b39eb9b1dd05c
Anshuman Sharma <ajrksharma@gmail.com>
2026-09-02 23:39:39 +0530

    Added LeetCode 3875: Construct Uniform Parity Array I

---
+++ Daily Challenge/3875_Construct_Uniform_Parity_Array_I.java
@@ -0,0 +1,21 @@
+/*
+ * LeetCode 3875 - Construct Uniform Parity Array I
+ * Daily Challenge
+ *
+ * Link: https://leetcode.com/problems/construct-uniform-parity-array-i/
+ *
+ * Approach:
+ * The required uniform parity can always be constructed,
+ * so the answer is always true.
+ *
+ * Therefore, no loop or array traversal is required.
+ *
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
